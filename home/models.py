c420cb0faa351ff9f0f70628f64cd8532a364049
App Generator <51070104+app-generator@users.noreply.github.com>
2025-03-10 22:41:26 +0200

    initial commit

---
+++ home/models.py
@@ -0,0 +1,43 @@
+# -*- encoding: utf-8 -*-
+"""
+Copyright (c) 2019 - present AppSeed.us
+"""
+
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+from django.utils.translation import gettext_lazy as _
+
+# Create your models here.
+
+class UserProfile(models.Model):
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    #__PROFILE_FIELDS__
+
+    #__PROFILE_FIELDS__END
+
+    def __str__(self):
+        return self.user.username
+    
+    class Meta:
+        verbose_name        = _("UserProfile")
+        verbose_name_plural = _("UserProfile")
+
+#__MODELS__
+class Service(models.Model):
+
+    #__Service_FIELDS__
+    title = models.CharField(max_length=255, null=True, blank=True)
+    created_by = models.CharField(max_length=255, null=True, blank=True)
+
+    #__Service_FIELDS__END
+
+    class Meta:
+        verbose_name        = _("Service")
+        verbose_name_plural = _("Service")
+
+
+
+#__MODELS__END
